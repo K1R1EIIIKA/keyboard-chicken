b05130c24b37c9fdde0e3c00305886431cab142c
VladIsLove16 <zalezshi@gmail.com>
2023-09-21 20:35:41 +0300

    +BaseCrumb +Chicken

---
+++ Assets/Scripts/BaseCrumb.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCrumb : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 0.1f;
+    public int Score = 0;
+    [SerializeField]
+    private float Size = 1;
+    public void GetHit()
+    {
+        Destroy(this.gameObject);
+    }
+    void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x - Speed * Time.fixedDeltaTime, transform.position.y);
+    }
+    public int GetScore() => Score;
+
+
+}
