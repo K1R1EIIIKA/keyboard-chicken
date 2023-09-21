68ef63673eb1cf31ee7cb216080019e12b648139
RanaBufo <milly.ili000@mail.ru>
2023-09-21 19:23:36 +0300

    dots

--- Assets/Scripts/dotDelete.cs
+++ Assets/Scripts/dotDelete.cs
@@ -6,10 +6,26 @@ public class dotDelete : MonoBehaviour
 {
 
     public float speed = 0.1f;
-    
+    int direction = -1;
+    private float[] speeder = { 1.1f, 1.3f, 1.2f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f };
     void FixedUpdate()
     {
-        transform.position = new Vector2(transform.position.x - speed * Time.fixedDeltaTime, transform.position.y);
+        if (direction == -1) {
+            direction = Random.Range(0, 3);
+        }
+        if (direction == 0)
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.fixedDeltaTime, transform.position.y);
+        }
+        else if (direction == 1)
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.fixedDeltaTime, transform.position.y - speeder[Random.Range(0,7)] * Time.fixedDeltaTime);
+        }
+        else if (direction == 2)
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.fixedDeltaTime, transform.position.y + speeder[Random.Range(0, 7)] * Time.fixedDeltaTime);
+        }
+
     }
 
    
--- Assets/Scripts/dotMove.cs
+++ Assets/Scripts/dotMove.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class dotMove : MonoBehaviour
 {
-    public GameObject[] dot;
+    public GameObject[] crumb;
+    public GameObject[] crumbWithMass;
     private float randomY;
     Vector2 whereToSpawn;
     public float spawnDelay;
     public float nextSpawn = 0.0f;
     public int cost = 3;
-    
+
     void Update()
     {
         if (Time.time > nextSpawn)
         {
+            GameObject Enemy;
+           
             for (int i = 0; i < Random.Range(1, cost); i++)
             {
-                nextSpawn = Time.time + spawnDelay;
-                randomY = Random.Range(-4.5f, 4.5f);
-                whereToSpawn = new Vector2(transform.position.x, randomY);
-                
-                GameObject Enemy = Instantiate(dot[Random.Range(0, dot.Length)], whereToSpawn, Quaternion.identity);
+                    nextSpawn = Time.time + spawnDelay;
+                    randomY = Random.Range(-4.5f, 4.5f);
+                    whereToSpawn = new Vector2(transform.position.x, randomY);
+                    Enemy = Instantiate(crumb[Random.Range(0, crumb.Length)], whereToSpawn, Quaternion.identity);
+                    
+
                 Destroy(Enemy, 6f);
+                
+                
+                
             }
         }
     }
