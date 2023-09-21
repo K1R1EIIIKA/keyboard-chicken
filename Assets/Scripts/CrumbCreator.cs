b8367a08091df6137578798f80f5cab21f990831
VladIsLove16 <zalezshi@gmail.com>
2023-09-21 16:53:50 +0300

    Renaming to Crumb

--- Assets/Scripts/CrumbCreator.cs
+++ Assets/Scripts/CrumbCreator.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dotMove : MonoBehaviour
+public class CrumbCreator : MonoBehaviour
 {
-    public GameObject[] dot;
+    public GameObject[] Crumbs;
+    public int CrumbTypes=2;
     private float randomY;
     Vector2 whereToSpawn;
-    public float spawnDelay;
+    public float spawnDelay=0.8f;
     public float nextSpawn = 0.0f;
-    public int cost = 3;
+    public int CrumbsToSpawnAmount = 3;
     
     void Update()
     {
         if (Time.time > nextSpawn)
         {
-            for (int i = 0; i < Random.Range(1, cost); i++)
+            for (int i = 0; i < Random.Range(1, CrumbsToSpawnAmount); i++)
             {
                 nextSpawn = Time.time + spawnDelay;
                 randomY = Random.Range(-4.5f, 4.5f);
                 whereToSpawn = new Vector2(transform.position.x, randomY);
-                
-                GameObject Enemy = Instantiate(dot[Random.Range(0, dot.Length)], whereToSpawn, Quaternion.identity);
-                Destroy(Enemy, 6f);
+                GameObject Crumb = Instantiate(Crumbs[Random.Range(0, CrumbTypes)], whereToSpawn, Quaternion.identity);
+                Destroy(Crumb, 6f);
             }
         }
     }
